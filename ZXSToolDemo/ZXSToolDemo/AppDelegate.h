d369f9e7dda53cadadb3ca9424bf66d7f3234a26
zxs <zhangxusong888@163.com>
2020-03-21 17:36:01 +0800

    添加Demo工程

---
+++ ZXSToolDemo/ZXSToolDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ZXSToolDemo
+//
+//  Created by zxs on 2020/3/21.
+//  Copyright © 2020 kjt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ ZXSToolDemo/ZXSToolDemo/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  ZXSToolDemo
+//
+//  Created by zxs on 2020/3/21.
+//  Copyright © 2020 kjt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
