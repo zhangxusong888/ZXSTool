9066ca6a0d6b0b776aa1c96c15fd0094ca67ac87
zxs <zhangxusong888@163.com>
2020-04-22 09:08:27 +0800

    地区model

--- ZXSTool/Classes/ZXSAreaSelector/ZXSAreaModel.h
+++ ZXSTool/Classes/ZXSAreaSelector/ZXSAreaModel.h
@@ -12,7 +12,22 @@ NS_ASSUME_NONNULL_BEGIN
 @interface ZXSAreaModel : NSObject
 
 // 省名称
-@property (copy, nonatomic) NSString *province;
+@property (copy, nonatomic) NSString *provinceName;
+
+// 省编码
+@property (copy, nonatomic) NSString *provinceCode;
+
+// 市名称
+@property (copy, nonatomic) NSString *cityName;
+
+// 市编码
+@property (copy, nonatomic) NSString *cityCode;
+
+// 区名称
+@property (copy, nonatomic) NSString *districtName;
+
+// 区编码
+@property (copy, nonatomic) NSString *districtCode;
 
 @end
 
